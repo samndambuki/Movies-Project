d52ff0f1c119d2b59bd0cd8502fb7311f40cf39e
samndambuki <sam.ndambuki08@gmail.com>
2023-01-06 17:13:45 +0300

    Edit Entity Error

---
+++ Frontend/react-movies/src/utils/EditEntity.tsx
@@ -0,0 +1,59 @@
+import axios, {AxiosResponse} from "axios";
+import { useEffect, useState } from "react";
+import { ReactElement } from "react-markdown/lib/react-markdown";
+import { useHistory, useParams } from "react-router-dom";
+import { GETGenresById } from "../endpoints";
+import { movieTheaterCreationDTO } from "../movietheatres/movieTheater.model";
+import DisplayErrors from "./DisplayErrors";
+import Loading from "./Loading";
+
+export default function EditEntity<TCreation, TRead>
+(props: editEntityProps<TCreation,TRead>)
+{
+  const { id }: any = useParams();
+  const [entity, setEntity] = useState<TCreation>();
+  const [errors, setErrors] = useState<string[]>([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    axios
+      .get(`${props.url}/${id}`)
+      .then((response: AxiosResponse<TRead>) => {
+        setEntity(props.transform(response.data));
+      });
+  }, [id]);
+
+  async function edit(entityToEdit: TCreation) {
+    try {
+      await axios.put(`${props.url}/${id}`, entityToEdit);
+      history.push(props.indexURL);
+    } catch (error) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
+    }
+  }
+
+    return(
+        <>
+        <h3>Edit {props.entityName}</h3>
+      <DisplayErrors errors={errors} />
+      {entity ? props.children(entity,edit) :
+        <Loading />
+      }
+      </>
+    )
+}
+
+interface editEntityProps<TCreation,TRead>
+{
+    url:string;
+    entityName: string;
+    indexURL: string;
+    transform(entity: TRead):TCreation;
+    //children(entity:TCreation, edit:(entity:TCreation) => void):ReactElement;
+}
+
+EditEntity.defautProps = {
+    transform:(entity:any) => entity
+}
